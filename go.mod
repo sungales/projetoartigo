cb2ceefee00344ff46209d1f3e874630e66f5dcf
sungales <caua.cruz610@gmail.com>
2026-05-08 17:15:33 -0300

    commit teste

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.26.2
 require modernc.org/sqlite v1.50.0
 
 require (
+	github.com/a-h/templ v0.3.1001 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 
 	"github.com/sungales/projetoartigo/routes"
+
 	database "github.com/sungales/projetoartigo/sql"
 	_ "modernc.org/sqlite"
+	
 )
 
 func main() {
@@ -15,6 +17,7 @@ func main() {
 	http.HandleFunc("GET /artigos", routes.GetArticlesRoute)
 	http.HandleFunc("POST /artigos/criar", routes.CreateArticleRoute)
 	http.HandleFunc("GET /artigos/{id}", routes.GetArticleByIDRoute)
+
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
